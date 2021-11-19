2e86e1d8ba5e35f07ff5ec699b070c47b9705fff
Ralph Donato <rdonato@albany.edu>
2021-11-19 07:32:34 -0500

    grip

---
+++ grip.py
@@ -0,0 +1,232 @@
+# Make sure to have CoppeliaSim running, with followig scene loaded:
+#grip handle gripping calcs etx
+# .ttt
+# If fail to connect try the following 
+# simRemoteApi.start(19999)
+# Do not launch simulation, then run this script
+#
+# The client side (i.e. this script) depends on:
+#
+# sim.py, simConst.py, and the remote API library available
+# in programming/remoteApiBindings/lib/lib
+# Additionally you will need the python math and msgpack modules
+
+#this is the procedure to get the inverse kinematics
+# it will exclude based on Angle of the actuator and a workspace.  Although not a true workspace
+
+
+
+
+
+
+
+try:
+    import sim
+except:
+    print ('--------------------------------------------------------------')
+    print ('"sim.py" could not be imported. This means very probably that')
+    print ('either "sim.py" or the remoteApi library could not be found.')
+    print ('Make sure both are in the same folder as this file,')
+    print ('or appropriately adjust the file "sim.py"')
+    print ('--------------------------------------------------------------')
+    print ('simRemoteApi.start(19999)')
+#import sim
+import math
+from posixpath import join
+import msgpack
+import time
+import numpy as np
+
+class Client:
+    def __enter__(self):
+        sim.simxFinish(-1) # just in case, close all opened connections
+        self.id=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
+        return self
+    
+    def __exit__(self,*err):
+        sim.simxFinish(-1)
+        print ('Program ended')
+#define workspace
+#this will be the actual space the robot will work in
+# it isn't totally the correct workspace, because it will have to return to the basket and reset
+#x:0.22187452018260956 y:0.3554052412509918 z:0.45164451003074646
+xMin = 0.15
+xMax = 0.20
+yMin = 0.25
+yMax = 0.35
+zMin = 0.25
+zMax = 0.35
+useWorkSpace =True  # this variable will indicate that the reverse kinematics is only saving items for workspace 
+USEAngle     =True   # wheter resticting based on angle.
+OneValueSearch =False  # this is to be used if testing for only one value
+valtoSearch=[]
+#define orientations to use
+#this are min max in degrees
+xMaxD =20
+xMinD =-20
+yMaxD =20
+yMinD =-20
+zMaxD =20
+zMinD =-20
+
+
+with Client() as client:
+    #INverseKinematics file
+     
+    #constants
+    sIntv =0.000000001#.0625
+    def degToRad(deg  ):
+        return deg* math.pi/180
+    def radToDeg(rad):
+        return rad*180/math.pi
+
+    def TrackJointh(hndl):
+        i=-180
+        while i<180:
+            
+            sim.simxSetJointPosition(client.id,hndl,i*math.pi/180,sim.simx_opmode_blocking)
+            c,pos = sim.simxGetJointPosition(client.id,hndl,sim.simx_opmode_blocking)
+            lc =getArmPos()
+            print (lc)
+            print( "jp = ",i, c, radToDeg(   pos)   )
+            i +=1
+            #time.sleep(sIntv)
+    def TrackJoint(jname):
+        print (jname)
+        jnt =joints[jname]
+        print(jnt)
+        i=jnt["min"]
+        while i<=jnt["max"]:
+            
+            sim.simxSetJointPosition(client.id,jnt["handle"],i*math.pi/180,sim.simx_opmode_blocking)
+            c,pos = sim.simxGetJointPosition(client.id,jnt["handle"],sim.simx_opmode_blocking)
+            lc =getArmPos()
+            print ("i {}|p {}|arm {} ".format(i,radToDeg(   pos),lc))
+            #print( "jp = ",i, c, radToDeg(   pos)   )
+            i +=1
+            #time.sleep(sIntv)
+              
+    def getArmPos():
+        st, loc =sim.simxGetObjectPosition(client.id,rFingerh, -1,  sim.simx_opmode_blocking)
+        return loc
+
+    def getArmOrientation():
+        #st, loc =sim.simxGetObjectPosition(client.id,rFingerh, -1,  sim.simx_opmode_blocking)
+        st, orr=sim.simxGetObjectOrientation(client.id,rFingerh,-1,sim.simx_opmode_blocking)
+        return orr
+
+    
+
+    def movAll( aAll):
+        w=0
+        while w<len(aAll):
+            s = "j" + str(w+1)
+            jnt =joints[s]
+            sim.simxSetJointPosition(client.id,jnt["handle"],aAll[w]*math.pi/180,sim.simx_opmode_blocking)
+            w+=1
+
+    
+    def testWS ( jPos):
+        retval =True
+        if useWorkSpace:
+            if(OneValueSearch):
+                print ("ONE POINT")
+                #do one point search here
+                rng = .0005
+                #valtoSearch
+                if (jPos[0] > valtoSearch[0]-rng and jPos[0]< valtoSearch[0]+rng and
+                    jPos[1] > valtoSearch[1]-rng and jPos[1]< valtoSearch[1]+rng and
+                    jPos[2] > valtoSearch[2]-rng and jPos[2]< valtoSearch[0]+rng ):
+                    return True
+                else:
+                    return False
+            else:
+                #print("Using Workspace")
+                if jPos[0] > xMax or jPos[0]< xMin or jPos[1] > yMax or jPos[1]< yMin or jPos[2] > zMax or jPos[2]< zMin:
+                    return False 
+                #must check orientation of end actuattor
+                if(USEAngle):
+                    # x is rotational and can be ignored .  need y and z only
+                    orAng = getArmOrientation()
+                    yangle = radToDeg  (orAng[1])
+                    zangle =radToDeg(orAng[2])
+
+                    if (  yangle<yMinD or yangle>yMaxD  or zangle<zMinD or zangle>zMaxD ):
+                        return False                                                                                               
+        return retval
+    def demoGRIPs():
+        print ("GRIP")
+        for i in np.arange(0,1.5,.05):
+            sim.simxSetJointPosition(client.id,griph,degToRad((i)),sim.simx_opmode_blocking)
+             
+            c,pos = sim.simxGetJointPosition(client.id,griph,sim.simx_opmode_blocking)
+            print(i,degToRad(i), radToDeg(pos))
+        print ("DONE GRIP")
+
+        print ("GRIPm")
+        for i in np.arange(0,1.5,.05):
+            sim.simxSetJointPosition(client.id,griphmh,degToRad((i)),sim.simx_opmode_blocking)
+             
+            c,pos = sim.simxGetJointPosition(client.id,griphmh,sim.simx_opmode_blocking)
+            print(i,degToRad(i) , radToDeg(pos))
+        print ("DONE GRIP")
+        sim.simxSetJointPosition(client.id,griphmh,-2,sim.simx_opmode_blocking)
+             
+        c,pos = sim.simxGetJointPosition(client.id,griphmh,sim.simx_opmode_blocking)
+        print(pos , radToDeg(pos))
+    
+    
+    print("running")
+
+    if client.id!=-1:
+        print ('Connected to remote API server')
+        Yumi= 'YUMI'
+        rFinger = "gripper_r_finger_r_visual"
+        #get joinds store in massive dict
+        ec,rFingerh =sim.simxGetObjectHandle(client.id,rFinger,sim.simx_opmode_blocking)
+        print( "ecf",ec)
+        ec,Yumih = sim.simxGetObjectHandle(client.id,Yumi,sim.simx_opmode_blocking)
+        print ("ecy",ec)
+        #Gripper Joints
+        grip  = "gripper_r_joint"
+        ec,griph = sim.simxGetObjectHandle(client.id,grip,sim.simx_opmode_blocking)
+        gripm = "gripper_r_joint_m"
+        ec,griphmh = sim.simxGetObjectHandle(client.id,gripm,sim.simx_opmode_blocking)
+
+
+
+
+        joints ={}
+        joints ["j1"] ={"name":"yumi_joint_1_r", "handle":0, "min":-169,"max":169}
+        joints ["j2"] ={"name":"yumi_joint_2_r", "handle":0, "min":-144,"max": 44}
+        joints ["j3"] ={"name":"yumi_joint_3_r", "handle":0, "min":-124,"max": 80  }
+        joints ["j4"] ={"name":"yumi_joint_4_r", "handle":0, "min":-290  ,"max":290  }
+        joints ["j5"] ={"name":"yumi_joint_5_r", "handle":0, "min":-88   ,"max":138  }
+        joints ["j6"] ={"name":"yumi_joint_6_r", "handle":0, "min":-229  ,"max":229  }
+        joints ["j7"] ={"name":"yumi_joint_7_r", "handle":0, "min":-169,"max":169}
+        i=1
+        while i<8:
+            s = "j" + str(i) 
+            ec,h   = sim.simxGetObjectHandle(client.id,joints[s]["name"],sim.simx_opmode_blocking)
+            if ec!=0:
+                print("Failed getting handles",ec ,s,joints[s]["name"])
+            joints[s]["handle"] = h
+            i+=1 
+        print ("Done Configuringg")
+
+        movAll([0,0,-90,0,-45,30,0])
+        demoGRIPs()
+        
+        
+        
+        sim.simxStopSimulation(client.id,sim.simx_opmode_blocking)
+        
+        #sim.simxGetStringSignal(client.id,client.stringSignalName1,sim.simx_opmode_discontinue)
+       
+        sim.simxGetPingTime(client.id)
+
+        # Now close the connection to CoppeliaSim:
+        sim.simxFinish(client.id)
+    else:
+        print ('Failed connecting to remote API server')
+        print('Did you try simRemoteApi.start(19999)')
